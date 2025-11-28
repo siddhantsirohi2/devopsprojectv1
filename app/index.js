e9ea2c71dd8b0ffb5c1d310536d31b9e96d93dc3
Siddhant Sirohi <120754700+siddhantsirohi2@users.noreply.github.com>
2025-11-28 22:45:31 +0530

    changing bg color to purple

--- app/index.js
+++ app/index.js
@@ -17,7 +17,7 @@ app.get('/', (req, res) => {
       <style>
         body {
           font-family: 'Segoe UI', sans-serif;
-          background-color:rgb(34, 232, 84);
+          background-color:rgb(126, 34, 232);
           color: #f8fafc;
           display: flex;
           justify-content: center;
